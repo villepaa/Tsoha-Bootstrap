7c73ca2ce0f6d97f6a65e2af60e708dad339dd5d
Ville Paavola <villepaa@dhcp-asv-87.helsinki.fi>
2016-08-08 20:09:22 +0300

    viikon 2 palautus

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,49 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Week VALUES (31);
+
+INSERT INTO Week VALUES (32);
+
+INSERT INTO Dates VALUES('2016-08-01',31);
+
+INSERT INTO Dates VALUES('2016-08-02',31);
+
+INSERT INTO Dates VALUES('2016-08-03',31);
+
+INSERT INTO Dates VALUES('2016-08-04',31);
+
+INSERT INTO Dates VALUES('2016-08-05',31);
+
+INSERT INTO Dates VALUES('2016-08-06',31);
+
+INSERT INTO Dates VALUES('2016-08-07',31);
+
+
+
+INSERT INTO Planner (name,password) VALUES ('setä suunnittelija','setä1');
+
+INSERT INTO Employee (id,forename,surname,osoite,puh) VALUES ('382738','Teppo', 'Duunari','Kuutamokuja 6 ,00101, Helsinki','038234550');
+
+INSERT INTO Task (id,alkuaika,loppuaika,kesto,tietoja) VALUES ('KV','08:00:00','19:00:00','11:00:00', 'Tietoja vuoroon liittyen');
+
+INSERT INTO Task (id,alkuaika,loppuaika,kesto,tietoja) VALUES ('OL','08:00:00','19:00:00','11:00:00', 'Tähänkin tietoja');
+
+INSERT INTO Weekday (task_id,numero) VALUES ('KV',1);
+
+INSERT INTO Weekday (task_id,numero) VALUES ('KV',2);
+
+INSERT INTO PlanBlock VALUES('Viikot: 31-32');
+
+
+INSERT INTO Weeks VALUES('Viikot: 31-32',31);
+
+INSERT INTO Weeks VALUES('Viikot: 31-32',32);
+
+INSERT INTO Plan (employee_id, task_id, day_of_task,planBlock_id,planner_id) VALUES (382738,'KV','2016-08-01','Viikot: 31-32',1);
+
+INSERT INTO Qualification (employee_id, task_id) VALUES(382738,'OL');
+
+
+
+
+
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,74 @@
+
+
+
+CREATE TABLE Week(
+    week_number INTEGER PRIMARY KEY
+);
+
+CREATE TABLE PlanBlock (
+    tunnus varchar PRIMARY KEY
+    
+);
+
+CREATE TABLE Dates(
+    id Date,
+    week_number INTEGER REFERENCES Week(week_number),
+    PRIMARY KEY(id, week_number) 
+);
+
+CREATE TABLE Weeks(
+    block_tunnus varchar REFERENCES PlanBlock(tunnus),
+    week_number INTEGER REFERENCES Week(week_number),
+    PRIMARY KEY(block_tunnus, week_number)
+);
+
+CREATE TABLE Employee(
+   
+    id INTEGER PRIMARY KEY NOT NULL,
+    forename varchar(50) NOT NULL,
+    surname varchar(50) NOT NULL,
+    osoite varchar(100),
+    puh varchar (20)
+);
+
 CREATE TABLE Planner(
     id SERIAL PRIMARY KEY,
     name varchar(50) NOT NULL,
     password varchar(50) NOT NULL
 );
+
+CREATE TABLE Task(
+    id varchar(10) PRIMARY KEY NOT NULL,
+    alkuaika Time,
+    loppuaika Time,
+    kesto Interval,
+    tietoja varchar(200)
+);
+
+CREATE TABLE Plan(
+    employee_id INTEGER REFERENCES Employee(id),
+    task_id varchar  REFERENCES Task(id),
+    day_of_task Date,
+    planBlock_id varchar REFERENCES PlanBlock(tunnus),
+    planner_id INTEGER REFERENCES Planner(id),
+    PRIMARY KEY(employee_id,task_id,day_of_task)
+    
+);
+
+CREATE TABLE Qualification(
+    employee_id INTEGER REFERENCES Employee(id),
+    task_id varchar REFERENCES Task(id),
+    PRIMARY KEY(employee_id,task_id)
+);
+
+CREATE TABLE Weekday(
+    id SERIAL PRIMARY KEY,
+    task_id varchar REFERENCES Task(id),
+    numero INTEGER NOT NULL
+);
+
+
+
+
+
 
